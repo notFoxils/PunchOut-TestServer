057bba7dc67799895d8b8e44b31ec7054f687288
Foxils <voltrixz9514@gmail.com>
2023-12-02 03:13:08 +0000

    add the other stuff

---
+++ src/client/client.CameraScript.lua
@@ -0,0 +1,49 @@
+local cam = workspace.CurrentCamera
+local Players = game:GetService("Players")
+local ts = game:GetService("TweenService")
+task.wait(4)
+local hurmps = {Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")}
+
+local xoffset = 0
+local yoffset = 2
+local zoffset = 40
+
+local camPart = Instance.new("Part")
+camPart.Anchored = true
+camPart.Name = "CamPart"
+camPart.Parent = workspace
+camPart.Transparency = 1
+camPart.CanCollide = false
+
+function calculateAveragePosition()
+	local total = Vector3.new()
+	for _, humrootpart in pairs(hurmps) do    
+		total += humrootpart.Position
+	end
+	return total / #hurmps
+end
+
+function calculateAverageMagnitude()
+	local total = 0
+	for _, humrootpart in pairs(hurmps) do    
+		total += (humrootpart.Position - camPart.Position).Magnitude
+	end
+
+	return total / #hurmps
+end
+
+function camManager()
+	local averagePos = calculateAveragePosition()
+	camPart.Position = averagePos
+	local averageMagnitude = calculateAverageMagnitude() + zoffset
+	local tweenInfo = TweenInfo.new(0.02, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut)
+	local propertyTable = {CFrame = camPart.CFrame * CFrame.new(xoffset, yoffset, averageMagnitude)}
+	
+	ts:Create(cam, tweenInfo, propertyTable):Play()
+end
+
+task.wait()
+cam.CameraType = Enum.CameraType.Scriptable
+
+game:GetService("RunService").Heartbeat:Connect(camManager)
+
