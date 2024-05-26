005b290f1c232ecc80fdd99dd37a76aab4730b1f
notFoxils <voltrixz9514@gmail.com>
2024-05-26 19:48:39 -0400

    yes

--- src/StarterPlayer/StarterPlayerScripts/ControlScript.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/ControlScript.client.lua
@@ -2,13 +2,25 @@ local RunService = game:GetService("RunService")
 local ContextActionService = game:GetService("ContextActionService")
 local UIS = game:GetService("UserInputService")
 local Players = game:GetService("Players")
+local localPlayer = Players.LocalPlayer
 
-local humanoidRootPart = Players.LocalPlayer.CharacterAdded:Wait():WaitForChild("HumanoidRootPart")
+local playerGui = localPlayer:WaitForChild("PlayerGui")
+local debugFrame = playerGui:WaitForChild("DebugGui").DebugFrame
+local debugValues = debugFrame:GetDescendants()
+
+local humanoidRootPart = localPlayer.Character:WaitForChild("HumanoidRootPart")
 
 local leftValue, rightValue = 0, 0
+local isJumping = false
+local jumpSquatFrames = 3
+local framesSinceJump = 0
+local timesJumped = 0
+local inAir = false
 local movementDirection:Vector2 = Vector2.new(rightValue + leftValue, 0)
 local walkingSpeed = 960/28
 local runningSpeed = 1320/28
+local jumpForce = 3.68
+local shortHopForce = 2.1
 
 local function handleMovementX()
 	if math.abs(movementDirection.X) >= 0.8 and UIS:IsKeyDown(Enum.KeyCode.Space) == false then
@@ -28,21 +40,69 @@ local function right(_actionName, inputState, _inputObject:InputObject)
     rightValue = (inputState == Enum.UserInputState.Begin) and 1 or 0
 end
 
-local function onJump(_actionName, _inputState, _inputObject:InputObject)
-
+local function onJump(_actionName, inputState, _inputObject:InputObject)
+	if inputState == Enum.UserInputState.Begin then
+		isJumping = true
+	elseif inputState == Enum.UserInputState.End then
+		isJumping = false
+		timesJumped += 1
+	end
 end
 
-local function onRenderStep(delta)
+local function moveThePlayer(delta:number)
 	movementDirection = Vector2.new(rightValue + leftValue, 0)
 
 	if movementDirection ~= Vector2.zero then
-		local moveBy = handleMovementX() * delta
-		humanoidRootPart:PivotTo(humanoidRootPart.CFrame * CFrame.new(Vector3.new(moveBy)))
+		humanoidRootPart:PivotTo(humanoidRootPart.CFrame * CFrame.new(Vector3.new(handleMovementX() * delta)))
 	end
+end
+
+local function handleJumpForce()
+	if movementDirection.Y == 0 then
+		if not isJumping and framesSinceJump > 0 then
+			if framesSinceJump <= jumpSquatFrames then
+				movementDirection = Vector2.new(movementDirection.X, shortHopForce)
+				print("short")
+			elseif framesSinceJump >= jumpSquatFrames then
+				movementDirection = Vector2.new(movementDirection.X, jumpForce)
+				print("full")
+			end
+			framesSinceJump = 0
+		elseif isJumping then
+			framesSinceJump += 1
+			if framesSinceJump > jumpSquatFrames then
+				isJumping = false
+			end
+		end
+	end
+end
+
+local function debugDisplay()
+	for index, value:TextLabel in pairs(debugValues) do
+		if value.Parent.ClassName == "TextLabel" then
+			if index == 2 then
+				value.Text = leftValue
+			elseif index == 4 then
+				value.Text = rightValue
+			elseif index == 6 then
+				value.Text = tostring(isJumping)
+			elseif index == 8 then
+				value.Text = tostring(framesSinceJump)
+			elseif index == 10 then
+				value.Text = tostring(movementDirection)
+			end
+		end
+	end
+end
+
+local function onRenderStep(delta)
+	moveThePlayer(delta)
+	handleJumpForce()
+	debugDisplay()
 end
 
 ContextActionService:BindAction("Left", left, false, Enum.KeyCode.A)
 ContextActionService:BindAction("Right", right, false, Enum.KeyCode.D)
 ContextActionService:BindAction("Jump", onJump, false, Enum.KeyCode.Space, Enum.KeyCode.W)
 
-RunService.PreRender:Connect(onRenderStep)
+RunService.RenderStepped:Connect(onRenderStep)
