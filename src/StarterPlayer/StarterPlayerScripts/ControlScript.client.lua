9b2b8ab69604cc9d1fe47473f4738ff4865145c2
notFoxils <voltrixz9514@gmail.com>
2024-05-29 21:17:12 -0400

    Refactored X movement Y tbd

--- src/StarterPlayer/StarterPlayerScripts/ControlScript.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/ControlScript.client.lua
@@ -1,6 +1,6 @@
 local RunService = game:GetService("RunService")
 local ContextActionService = game:GetService("ContextActionService")
-local UIS = game:GetService("UserInputService")
+local UserInputService = game:GetService("UserInputService")
 local Players = game:GetService("Players")
 local localPlayer = Players.LocalPlayer
 
@@ -10,133 +10,112 @@ local debugValues = debugFrame:GetDescendants()
 
 local humanoidRootPart = localPlayer.Character:WaitForChild("HumanoidRootPart")
 
-local leftValue, rightValue = 0, 0
+local leftInput, rightInput = 0, 0
 local isJumping = false
-local jumpSquatFrames = 3
 local framesSinceJump = 0
-local timesJumped = 0
+local doubleJumpPossible = true
 local inAir = false
-local lastJump = "none"
-local movementDirection:Vector2 = Vector2.new(rightValue + leftValue, 0)
+local velocity:Vector2 = Vector2.new(0, 0)
+local currentFallingSpeed = 0
+
+--statics, currently based off of converted values from fox in melee,should be modularized, but problem for later me :)
 local walkingSpeed = 960/28 --Studs/Frame
 local runningSpeed = 1320/28 --Studs/Frame
 local jumpForce = (3.68/2.8)*60 --(Studs/Frame)*60 || Studs/Frame = (SU (Smash Unit)/Frame)/2.8
 local shortHopForce = (2.1/2.8)*60 --(Studs/Frame)*60 || Studs/Frame = (SU (Smash Unit)/Frame)/2.8
 local gravity = (0.23/2.8)*60 --(Studs/Frame)*60 || Studs/Frame = (SU (Smash Unit)/Frame)/2.8
 local fallingSpeed = (2.8/2.8)*60 --(Studs/Frame)*60 || Studs/Frame = (SU (Smash Unit)/Frame)/2.8
-local currentFallingSpeed = 0
+local jumpSquatFrames = 3
+
+--debug
+local lastJump = "none"
+
+--temporoary, to be solved/implemented later ingame though menus and whatnot
+
+
+local function isGrounded()
+	local raycastResult = workspace:Raycast(humanoidRootPart.Position, Vector3.new(0, -1, 0), RaycastParams.new())
+
+	if not raycastResult == nil then
+		return true
+	end
+	return false
+end
+
+local function left(_actionName, inputState, _inputObject:InputObject)
+	leftInput = (inputState == Enum.UserInputState.Begin) and -1 or 0
+end
+
+local function right(_actionName, inputState, _inputObject:InputObject)
+    rightInput = (inputState == Enum.UserInputState.Begin) and 1 or 0
+end
+
+local function onJump(_actionName, inputState, _inputObject:InputObject)
+	local currentGroundedStatus = isGrounded()
+
+	if inputState == Enum.UserInputState.Begin and (currentGroundedStatus or doubleJumpPossible) then
+		doubleJumpPossible = (currentGroundedStatus) and true or false
+		isJumping = true
+	elseif inputState == Enum.UserInputState.End then
+		isJumping = false
+	end
+end
+
+local function horizontalInput()
+	return leftInput + rightInput
+end
 
 local function handleMovementX()
-	if math.abs(movementDirection.X) >= 0.2875 or UIS:IsKeyDown(Enum.KeyCode.Space) == true then
-		return walkingSpeed * movementDirection.X
-	elseif math.abs(movementDirection.X) >= 0.8 and UIS:IsKeyDown(Enum.KeyCode.Space) == false then
-		return runningSpeed * movementDirection.X
+	local currentInputX = horizontalInput()
+
+	if math.abs(currentInputX) >= 0.2875 or UserInputService:IsKeyDown(Enum.KeyCode.Space) == true then --The keycode should be user configureable later on
+		return walkingSpeed * currentInputX
+	elseif math.abs(currentInputX) >= 0.8 and UserInputService:IsKeyDown(Enum.KeyCode.Space) == false then --The keycode should be user configureable later on
+		return runningSpeed * currentInputX
 	else
 		return 0
 	end
 end
 
 local function handleMovementY()
-	if currentFallingSpeed > 0 then
-		currentFallingSpeed = 0
-	elseif currentFallingSpeed < fallingSpeed and inAir then
-		currentFallingSpeed += gravity
-		if currentFallingSpeed >= fallingSpeed then
-			currentFallingSpeed = fallingSpeed
-			inAir = false
-		end
-	end
-
-	if movementDirection.Y > 0 then
-		movementDirection = Vector3.new(movementDirection.X, movementDirection.Y - currentFallingSpeed)
-		return movementDirection.Y
-	end
-
-	return 0
+	
 end
 
-local function left(_actionName, inputState, _inputObject:InputObject)
-	leftValue = (inputState == Enum.UserInputState.Begin) and -1 or 0
+local function updateVelocity(delta:number)
+	velocity = Vector2.new(handleMovementX() * delta)--, handleMovementY() * delta)
 end
 
-local function right(_actionName, inputState, _inputObject:InputObject)
-    rightValue = (inputState == Enum.UserInputState.Begin) and 1 or 0
-end
-
-local function onJump(_actionName, inputState, _inputObject:InputObject)
-	if timesJumped >= 2 and inAir == false then
-		timesJumped = 0
-	end
-
-	if timesJumped < 2 then
-		if inputState == Enum.UserInputState.Begin then
-			isJumping = true
-		elseif inputState == Enum.UserInputState.End then
-			isJumping = false
-			timesJumped += 1
-		end
-	end
-end
-
-local function moveThePlayer(delta:number)
-	movementDirection = Vector2.new(rightValue + leftValue, movementDirection.Y)
-
-	if movementDirection ~= Vector2.zero then
-		humanoidRootPart:PivotTo(humanoidRootPart.CFrame * CFrame.new(Vector3.new(handleMovementX() * delta, handleMovementY() * delta)))
-	end
-end
-
-local function handleJumpForce()
-	if movementDirection.Y == 0 or inAir then
-		if not isJumping and framesSinceJump > 0 then
-			if framesSinceJump <= jumpSquatFrames then
-				movementDirection = Vector2.new(movementDirection.X, shortHopForce)
-				inAir = true
-				lastJump = "short"
-			elseif framesSinceJump >= jumpSquatFrames then
-				movementDirection = Vector2.new(movementDirection.X, jumpForce)
-				inAir = true
-				lastJump = "full"
-			end
-
-			framesSinceJump = 0
-		elseif isJumping then
-			framesSinceJump += 1
-
-			if framesSinceJump > jumpSquatFrames then
-				isJumping = false
-			end
-		end
-	end
+local function updatePosition()
+	humanoidRootPart:PivotTo(humanoidRootPart.CFrame * CFrame.new(Vector3.new(velocity.X, velocity.Y, 0))) --multiplying cframes is like adding regular numebers, confusing, but "it is what it is"
 end
 
 local function debugDisplay()
 	for index, value:TextLabel in pairs(debugValues) do
 		if value.Parent.ClassName == "TextLabel" then
 			if index == 2 then
-				value.Text = leftValue
+				value.Text = leftInput
 			elseif index == 4 then
-				value.Text = rightValue
+				value.Text = rightInput
 			elseif index == 6 then
 				value.Text = tostring(isJumping)
 			elseif index == 8 then
 				value.Text = tostring(framesSinceJump)
 			elseif index == 10 then
-				value.Text = tostring(movementDirection)
+				value.Text = tostring(velocity)
 			elseif index == 12 then
 				value.Text = lastJump
 			elseif index == 14 then
 				value.Text = currentFallingSpeed
 			elseif index == 16 then
-				value.Text = tostring(inAir)
+				value.Text = tostring(isGrounded)
 			end
 		end
 	end
 end
 
 local function onRenderStep(delta)
-	handleJumpForce()
-	moveThePlayer(delta)
+	updateVelocity(delta)
+	updatePosition()
 	debugDisplay()
 end
 
